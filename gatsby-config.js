eb354ec04dd8611ce558515b2e6aab6368f8a831
Veeral Shah <veeralshah@Veerals-MacBook-Pro.local>
2020-02-14 13:38:45 -0800

    add logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: `static/logo.jpg`, // This path is relative to the root of the site.
+        icon: `static/logo.png`, // This path is relative to the root of the site.
       },
     },
   ],
