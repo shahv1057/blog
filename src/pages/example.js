52c94f83dccbc5db5cbf41a2c6ce901586a83a0f
Kunal <me@kunal.sh>
2020-04-08 19:20:34 -0700

    simplify react example

--- src/pages/example.js
+++ src/pages/example.js
@@ -1,20 +1,18 @@
 import React from "react"
 import Plot from "react-plotly.js"
 
-export default ({}) => {
-  return (
-    <Plot
-      data={[
-        {
-          x: [1, 2, 3],
-          y: [2, 6, 3],
-          type: "scatter",
-          mode: "lines+markers",
-          marker: { color: "red" },
-        },
-        { type: "bar", x: [1, 2, 3], y: [2, 5, 3] },
-      ]}
-      layout={{ width: 320, height: 240, title: "A Fancy Plot" }}
-    />
-  )
-}
+export default ({}) => (
+  <Plot
+    data={[
+      {
+        x: [1, 2, 3],
+        y: [2, 6, 3],
+        type: "scatter",
+        mode: "lines+markers",
+        marker: { color: "red" },
+      },
+      { type: "bar", x: [1, 2, 3], y: [2, 5, 3] },
+    ]}
+    layout={{ width: 320, height: 240, title: "A Fancy Plot" }}
+  />
+)
